13f33ecce55cb16370af47c53677a5bbd5151086
Your Name <ujumaniyozov@ya.ru>
2020-07-27 22:26:06 +0500

    Product

--- controllers/productController.js
+++ controllers/productController.js
@@ -14,22 +14,16 @@ exports.addProduct = async (req, res) => {
     const product = new Product({
         nameUz: req.body.nameUz,
         nameRu: req.body.nameRu,
-        pid: Date.now(),
         category: req.body.category,
         size: req.body.size,
-        netto: req.body.netto,
         diametr: req.body.diametr,
-        diz: req.body.diz,
-        pok: req.body.pok,
         descriptionUz: req.body.descriptionUz,
         descriptionRu: req.body.descriptionRu,
         xarakterUz: req.body.xarakterUz,
         xarakterRu: req.body.xarakterRu,
-        video: req.body.video,
-        instruksiyaUz: req.body.instruksiyaUz,
-        instruksiyaRu: req.body.instruksiyaRu,
         images: urls,
         price: req.body.price,
+        pid: Date.now(),
         date: Date.now()
     });
 
--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 const slug = require('mongoose-slug-generator');
 const Schema = mongoose.Schema;
 mongoose.plugin(slug);
@@ -14,17 +15,11 @@ const productSchema = new Schema({
         required: true
     },
     size: {type: String, required: true},
-    netto: {type: String, required: true},
     diametr: {type: String, required: true},
-    diz: {type: String, required: true},
-    pok: {type: String, required: true},
     descriptionUz: {type: String,required: true},
     descriptionRu: {type: String,required: true},
     xarakterUz: {type: String, required: true},
     xarakterRu: {type: String, required: true},
-    video: {type: String, required: true},
-    instruksiyaUz: {type: String, required: true},
-    instruksiyaRu: {type: String, required: true},
     images: [
         {
             url : {
