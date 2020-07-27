7d4ec6a78c9c2a96fd394b408b8092419e2d06b0
rbcoder12345 <68811843+rbcoder12345@users.noreply.github.com>
2020-07-27 19:11:41 +0500

    Update color.js

--- models/color.js
+++ models/color.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const colorSchema = Schema({
+    url: {type: String, required: true},
     name : String,
     date: {type: Date, required: true}
 });
