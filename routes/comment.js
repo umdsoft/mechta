47097610af022593ab3393f30d3918f5b2697be0
Your Name <ujumaniyozov@ya.ru>
2020-07-30 16:46:17 +0500

    asd

--- routes/comment.js
+++ routes/comment.js
@@ -6,7 +6,7 @@ const {eA,eAdmin,eOperator,eBoth} = require('../middleware/checkUser');
 
 router.post('/comment',commentController.addComment)
 router.get('/comments',commentController.getAllNoActiveComments)
-router.get('/all-comments',commentController.getAllComments)
+router.get('/allcomments',commentController.getAllComments)
 router.get('/comment',commentController.getCommentByProduct)
 router.patch('/comment',commentController.updateCommentById)
 module.exports = router
